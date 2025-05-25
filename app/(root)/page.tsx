ae6a3a8804a32db4c7648ae747f26f2b3ebc53bc
Your Name <you@example.com>
2025-05-25 11:48:27 +0700

    form search

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,22 @@
+import React from 'react'
+import SearchForm from '../components/SearchForm';
 
-export default function Home() {
+const Home = async ({ searchParams }: { searchParams: { query: string } }) => {
+  const query = (await searchParams)?.query || '';
   return (
     <>
-      <h1 className="text-2xl">Home</h1>
+      <section className="pink_container">
+        <h1 className="heading">
+          Pitch Your Startup, <br />
+          Connect with Entrepreneurs
+        </h1>
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions
+        </p>
+        <SearchForm query={query} />
+      </section>
     </>
   );
 }
+
+export default Home
