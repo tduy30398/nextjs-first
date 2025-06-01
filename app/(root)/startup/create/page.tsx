2cc454009eff5335d87a3ea4c6df84eab303160b
Your Name <you@example.com>
2025-06-01 22:48:57 +0700

    create author

---
+++ app/(root)/startup/create/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Page = () => {
+    return (
+        <>
+            <div className="pink_container !min-h-[230px]">
+                <h1 className="heading">Submit Your Startup</h1>
+            </div>
+        </>
+    )
+}
+
+export default Page
--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -22,3 +22,14 @@ export const STARTUP_VIEWS_QUERY = defineQuery(`*[_type == "startup" && _id == $
   _id, views
 }
 `)
+
+export const AUTHOR_BY_GITHUB_ID_QUERY = defineQuery(`*[_type == "author" && id == $id][0] {
+  _id,
+  id,
+  name,
+  image,
+  bio,
+  userName,
+  email
+}
+`)
